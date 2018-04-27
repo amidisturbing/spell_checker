36c9094f341f19d1e44bd03416faaf53e4b42b3d
Rafa <rafa@Rafas-Air.fritz.box>
2018-04-28 01:32:00 +0200

    repo removed

--- src/ol_blob.py
+++ src/ol_blob.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 import csv
 import re
-datasource = 'comments_toxic.train'
+datasource = 'labeled_data_ol_short.csv'
 rowname = 'comment_text'
 with open('/Users/rafa/spell_checker/data/'+datasource, newline='') as csvfile:
      reader = csv.DictReader(csvfile)
@@ -11,7 +11,13 @@ with open('/Users/rafa/spell_checker/data/'+datasource, newline='') as csvfile:
      	corrected = blob.correct()
      	print(corrected)
      	#match user id  of offensive language data set
-     	
+     	matchObj = re.match( r'(\@(.*)\:)', corrected, re.M|re.I)
+     	if matchObj:
+     		print ("matchObj.group() : ", matchObj.group())
+     		print ("matchObj.group(1) : ", matchObj.group(1))
+     		print ("matchObj.group(2) : ", matchObj.group(2))
+     	else:
+     		print ("No match!!")
 	
 	#line = "@X_XPDOTJDOT: Damn near gotta protect my tweets.. stalkers being annoying you got that dope dick. Drive these bitches loco"
 	#print(re.sub(r'(\@(.*)\:)','', line))
