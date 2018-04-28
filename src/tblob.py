df5d2f369f2d98dab1f5928de7e9f33bdd194ca5
Rafa <rafa@Rafas-Air.fritz.box>
2018-04-29 01:58:34 +0200

    shorter test set added

--- src/tblob.py
+++ src/tblob.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 import csv
 import re
-datasource = 'comments_toxic.train'
+datasource = 'comments_toxic.short'
 rowname = 'comment_text'
 outf = 'tblob_temp'
 
@@ -15,7 +15,7 @@ with open('/Users/rafa/spell_checker/data/'+datasource, newline='') as csvfile :
           rows_corrected += row['id'] + row['comment_text'] + row ['toxic']+'\n'
           #print (rows_corrected)
 
-with open('results_spell_checker.csv', 'w') as csv_file:
+with open('/Users/rafa/spell_checker/data/results_spell_checker.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerow(['id','comment_text', 'toxic'])
     for key, value in rows_corrected.items():
